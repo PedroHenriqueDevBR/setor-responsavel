9ca593864157b863720d81adbc6af7a7c3d7461d
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-11-18 20:27:14 -0300

    feta: updated user datails

--- backend/project/apps/users/urls.py
+++ backend/project/apps/users/urls.py
@@ -4,6 +4,7 @@ from apps.users.views import auth, user_views, subsidiary_views
 urlpatterns = [
     # Users
     path("", user_views.ListUserView.as_view(), name="users"),
+    path("<int:pk>/edit", user_views.EditUserView.as_view(), name="user_edit"),
     # Auth
     path("login", auth.Login.as_view(), name="login"),
     path("logout", auth.Logout.as_view(), name="logout"),
